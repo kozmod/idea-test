4d80ac2aa08eccfb6b87699693bb5579e23a1863
18388871 <kozmo23g@gmail.com>
2020-06-25 16:24:39 +0300

    add Mockito @Spy Test

---
+++ core-test/src/test/java/ru/idea/test/lib/mockito/SpyTest.java
@@ -0,0 +1,48 @@
+package ru.idea.test.lib.mockito;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Objects;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SpyTest {
+
+    @Spy
+    private ClassToSpying classToSpying = new ClassToSpying();
+
+    @Test
+    public void shouldWorkWithoutOverriding() {
+        var inA = "A";
+        var inB = "B";
+        var res = inA + inB;
+        assertEquals(res, classToSpying.concat(inA, inB));
+    }
+
+    @Test
+    public void shouldWorkWithOverriding() {
+        var inA = "A";
+        var inB = "B";
+        var res = inA + inB;
+        when(classToSpying.concat(inA, inA)).thenReturn(res);
+        assertEquals(res, classToSpying.concat(inA, inB));
+        assertEquals(res, classToSpying.concat(inA, inA));
+        assertNotEquals(res, classToSpying.concat(inB, inA));
+    }
+}
+
+
+class ClassToSpying {
+
+    String concat(String inA, String inB) {
+        Objects.requireNonNull(inA);
+        Objects.requireNonNull(inB);
+        return inA.concat(inB);
+    }
+}
