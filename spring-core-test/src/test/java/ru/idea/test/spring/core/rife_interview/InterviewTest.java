a66f07139b5c27b195ff7ca36c53da14fff4f2c8
18388871 <kozmo23g@gmail.com>
2020-06-26 09:03:17 +0300

    rife interview test

--- spring-core-test/src/test/java/ru/idea/test/spring/core/rife_interview/InterviewTest.java
+++ spring-core-test/src/test/java/ru/idea/test/spring/core/rife_interview/InterviewTest.java
@@ -1,43 +1,57 @@
 package ru.idea.test.spring.core.rife_interview;
 
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.math.BigInteger;
+
+import static org.junit.Assert.assertEquals;
 
 public class InterviewTest {
 
     @Test
-    public void shouldLoadAnyConfigAndGetFoo() {
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AnyInterviewConfig.class);
-    }
-
-    public class TestClass extends Thread {
-        @Override
-        public void run() {
-            System.out.println("run");
-            throw new RuntimeException();
+    public void shouldLoadInRightOrder() throws IOException {
+        var old = System.out;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(baos, true);
+        System.setOut(ps);
+        try (baos; ps) {
+            new AnnotationConfigApplicationContext(AnyInterviewConfig.class);
         }
-
+        assertEquals("1234", baos.toString());
+        System.setOut(old);
     }
+
 
     @Test
-    public void name2() {
+    public void shouldThrowException() {
+        class TestClass extends Thread {
+            @Override
+            public void run() {
+                System.err.println("RUN");
+                throw new RuntimeException("Uncatchable exception from another thread ");
+            }
+
+        }
+
         try {
             new TestClass().start();
-            System.out.println("main");
+            System.err.println("MAIN");
         } catch (RuntimeException e) {
-            System.out.println("catch");
+            System.err.println("CATCH");
         }
     }
 
+    @SuppressWarnings("ResultOfMethodCallIgnored")
     @Test
-    public void name() {
+    public void shouldReturnWithRoundDown() {
         BigInteger a = BigInteger.valueOf(239);
         a.add(BigInteger.valueOf(239));
         BigInteger b = BigInteger.valueOf(42);
         b.subtract(BigInteger.valueOf(42));
-        System.out.println(a.divide(b));
+        assertEquals(new BigInteger("5"), a.divide(b));
     }
 }
