40a9b5152919c3e9082011d855cf32c4ca4b3fa3
18388871 <kozmo23g@gmail.com>
2020-07-18 10:25:34 +0300

    add core CompletableFutureTest

--- core-test/src/test/java/ru/idea/test/core/concurrent/completablefuture/CompletableFutureTest.java
+++ core-test/src/test/java/ru/idea/test/core/concurrent/completablefuture/CompletableFutureTest.java
@@ -1,9 +1,11 @@
 package ru.idea.test.core.concurrent.completablefuture;
 
+import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
 import ru.idea.test.utils.ConcurrentUtils;
 
+import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
@@ -93,5 +95,73 @@ public class CompletableFutureTest {
                 = completableFuture.thenRun(() -> System.out.println("Example with thenRun()."));
         Void res = thenCompletableFuture.get(); //block here
         Assert.assertNull(res);
+    }
+
+    @Test
+    public void shouldRunMultipleFutureParallel() throws ExecutionException, InterruptedException {
+        CompletableFuture<String> future1
+                = CompletableFuture.supplyAsync(() -> "Hello");
+        CompletableFuture<String> future2
+                = CompletableFuture.supplyAsync(() -> "Beautiful");
+        CompletableFuture<String> future3
+                = CompletableFuture.supplyAsync(() -> "World");
+
+        CompletableFuture<Void> combinedFuture
+                = CompletableFuture.allOf(future1, future2, future3);
+
+        combinedFuture.get(); //block here
+
+        Assert.assertTrue(future1.isDone());
+        Assert.assertTrue(future2.isDone());
+        Assert.assertTrue(future3.isDone());
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Test
+    public void shouldHandleInnerException() throws ExecutionException, InterruptedException {
+        String name = null;
+        CompletableFuture<String> completableFuture
+                = CompletableFuture.supplyAsync(() -> {
+            if (name == null) {
+                throw new RuntimeException("Computation error!");
+            }
+            return "Hello, " + name;
+        }).handle((value, throwable) -> {
+            Assert.assertNotNull(throwable);
+            Assert.assertThat(throwable, Matchers.instanceOf(RuntimeException.class));
+            return value != null
+                    ? value
+                    : "Hello, Stranger!";
+        });
+        Assert.assertEquals("Hello, Stranger!", completableFuture.get());
+    }
+
+    @Test
+    public void shouldCompleteExceptionallyFromOtherThread() {
+        var ioExMsg = "Calculation failed!";
+        CompletableFuture<String> completableFuture = new CompletableFuture<>();
+        completableFuture.completeExceptionally(new IOException(ioExMsg));
+        completableFuture.handle((val,throwable) -> {
+            Assert.assertNull(val);
+            Assert.assertNotNull(throwable);
+            Assert.assertThat(throwable, Matchers.instanceOf(IOException.class));
+            System.err.println("CALL");
+            return "some res";
+        });
+        completableFuture.handle((val,throwable) -> {
+            System.err.println("CALL 2");
+            Assert.assertNotNull(val); //throw AssertionError in other thread
+            System.err.println("NOT CALL");
+            return "some res";
+        });
+        try {
+            completableFuture.get(); // ExecutionException
+            Assert.fail();
+        } catch (Exception ex) {
+            Assert.assertNotNull(ex);
+            Assert.assertThat(ex, Matchers.instanceOf(ExecutionException.class));
+            Assert.assertThat(ex.getCause(), Matchers.instanceOf(IOException.class));
+            Assert.assertEquals(ioExMsg, ex.getCause().getMessage());
+        }
     }
 }
